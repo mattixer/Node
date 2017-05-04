8050bf54bef6e5b42f8eb3bd1e79270bbb33f122
Matija Kučinac <matthouse.kucinac@gmail.com>
2017-05-04 08:52:52 +0200

    visina i širina prozora, i sve ono prije

---
+++ java.js
@@ -0,0 +1,39 @@
+/**
+ * Created by Ucenik on 4.5.2017..
+ */
+var x = document.getElementById("dodavanje");
+x.style.display="none";
+
+function myFunction() {
+    if(x.style.display === "none"){
+        x.style.display = "block";
+        document.getElementById("gumb").innerHTML = "Hide";
+    }else{
+        x.style.display = "none";
+        document.getElementById("gumb").innerHTML = "Show";
+    }
+}
+
+function dodaj() {
+    var para = document.createElement("p");
+    var node = document.createTextNode(document.getElementById("tekst").value);
+    para.appendChild(node);
+    var element = document.getElementById("ptag");
+    element.appendChild(para);
+    document.getElementById("tekst").value="";
+}
+
+function bojatron() {
+    var boja = document.getElementById("bojica").value;
+    var divide = document.getElementById("ptag");
+    divide.style.color = boja;
+    document.getElementById("sir").innerHTML=boja;
+    document.getElementById("bojica").value="";
+}
+
+function widith() {
+    var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    var h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+    document.getElementById("visina").innerHTML = h;
+    document.getElementById("sirina").innerHTML = w;
+}
